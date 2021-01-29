0cb2a87fa7462587b1cf822a794aa70741a01cc6
Carol Santos <hey@olacarol.com.br>
2021-01-28 21:06:26 -0300

    conteúdo aula 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
@@ -23,17 +25,32 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>{db.title}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:description" content={db.description} />
+        <meta property="og:title" content={db.title} />
+        <meta property="og:site_name" content={db.title} />
+        <meta property="og:url" content={db.siteURL} />
+        <meta property="og:image" content={db.bg} />
+        <meta property="og:type" content="website" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
-import IndexPage from '../src/components/IndexPage';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
 //   flex:1;
@@ -29,16 +27,37 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  let [name, setName] = React.useState('');
   return (
     <QuizBackground backgroundImage={db.bg}>
-      <IndexPage />
       <QuizContainer>
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={function (e) {
+              e.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do React'); 
+
+              // router manda para a próxima página
+            }}
+            >
+              <input
+                onChange={function (e) {
+                  // State
+                  // name = e.target.value;
+                  setName(e.target.value);
+                }}
+                placeholder="Digite aqui seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
